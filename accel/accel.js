26e142e93e795dacd7474bd689b28ae6934d2e4c
Koki Ota <ma20015@shibaura-it.ac.jp>
2021-01-27 20:50:12 +0900

    accelTets

---
+++ accel/accel.js
@@ -0,0 +1,11 @@
+var i2c = require('i2c-bus');
+
+const DEVICE_NUMBER = 1;
+const TARGET_IC_ADDR = 0x32;
+
+var i2c_dev = i2c.openSync(DEVICE_NUMBER);
+
+var readBuf = new Buffer.alloc(0x10);
+i2c_dev.i2cReadSync(TARGET_IC_ADDR, readBuf.length, readBuf);
+
+console.log(readBuf);
