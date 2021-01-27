4390291cce5648871788ac02cd3ab9ee81f4ff83
Koki Ota <ma20015@shibaura-it.ac.jp>
2021-01-28 02:00:26 +0900

    fix temp2

--- temp/temp2.js
+++ temp/temp2.js
@@ -5,5 +5,5 @@ const ACCEL_REG = 0x32;
 
 var i2c_dev = i2c.openSync(1);
 var readBuf = new Buffer.alloc(0x100)
-var rawData = i2c_dev.i2cReadSync(TARGET_IC_ADDR, readBuf.length, readBf);
+var rawData = i2c_dev.i2cReadSync(TARGET_IC_ADDR, readBuf.length, readBuf);
 console.log(readBuf);
