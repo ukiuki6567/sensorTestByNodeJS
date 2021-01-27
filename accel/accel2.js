41fd6b5e538b4e46888f66c7f3dd7bfee7cde817
Koki Ota <ma20015@shibaura-it.ac.jp>
2021-01-27 21:34:05 +0900

    accel ver2

---
+++ accel/accel2.js
@@ -0,0 +1,21 @@
+const ADXL345 = require('adxl-345-sernsor');
+const adxl345 = new ADXL345();
+
+const getAcceleration = () => {
+	adxl345.getAcceleration(true)
+		.then((acceleration) => {
+			console.log(`acceleration = ${JSON.stringify(acceleration, null, 2)}`);
+			setTimeout(getAcceleration, 1000);
+		})
+		.catch((err) => {
+			console.log(`ADXL345 read error: ${err}`);
+			setTimeout(getAcceleration, 2000);
+		});
+};
+
+adxl345.init()
+	.then(() => {
+		console.log('ADXL345 initialization suucceeded');
+		getAcceleration();
+	})
+	.catch((err) => console.log(`ADXL345 initialization failied: ${err}`));
